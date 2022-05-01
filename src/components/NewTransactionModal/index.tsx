a74f8699bf28bc2a2853d8e0ded72e6556e72265
atalo pereira <atalopereira@hotmail.com>
2022-05-01 16:01:01 -0300

    ✨ Add number and date format

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,11 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+
+import { api } from '../../services/api'
+
 import {
   Container,
   TransactionTypeContainer,
@@ -15,7 +18,21 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('')
+  const [value, setValue] = useState(0)
+  const [category, setCategory] = useState('')
   const [type, setType] = useState('deposit')
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault()
+    const data = {
+      title,
+      value,
+      category,
+      type
+    }
+    api.post('/transactions', data)
+  }
 
   return (
     <Modal
@@ -33,16 +50,20 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           <img src={closeImg} alt="Fechar modal" />
         </button>
 
-        <Container>
+        <Container onSubmit={handleCreateNewTransaction}>
           <h2>Cadastrar transação</h2>
 
           <input
             placeholder='Titulo'
+            value={title}
+            onChange={event => setTitle(event.target.value)}
           />
 
           <input
             type="number"
             placeholder='Valor'
+            value={value}
+            onChange={event => setValue(Number(event.target.value))}
           />
 
           <TransactionTypeContainer>
@@ -68,6 +89,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
           <input
             placeholder='Categoria'
+            value={category}
+            onChange={event => setCategory(event.target.value)}
           />
 
           <button type="submit">
--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,12 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
 
   useEffect(() => {
     api.get('transactions')
-      .then(response => console.log(response.data))
+      .then(response => setTransactions(response.data.transactions))
   }, [])
 
   return (
@@ -22,18 +32,27 @@ export function TransactionTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2022</td>
+        {transactions.map(transactions => (
+          <tr key={transactions.id}>
+            <td>{transactions.title}</td>
+            <td className={transactions.type}>
+              {
+                new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transactions.amount)
+              }
+            </td>
+            <td>{transactions.category}</td> 
+            <td>
+              {
+                new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transactions.createdAt)
+                )
+              }
+            </td>
           </tr>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="withdraw">- R$1.100</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2022</td>
-          </tr>
+        ))}
         </tbody>
       </table>
     </Container>
